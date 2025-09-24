509a5ebd4b44fce61c294e8ba1e4ff6a2f757745
Prateek <nprateek@gmail.com>
2025-09-24 16:03:04 +0530

    first commit

---
+++ terraform/modules/ecr/main.tf
@@ -0,0 +1,33 @@
+resource "aws_ecr_repository" "backend" {
+    name = "${var.environment}-todo-backend"
+
+    image_scanning_configuration {
+        scan_on_push = true
+    }
+}
+
+# resource "aws_ecr_repository" "name" {
+#     name = "${var.environment}-todo-backend"
+#     force_delete = true
+  
+# }
+
+resource "aws_ecr_lifecycle_policy" "ecr_policy" {
+    repository = aws_ecr_repository.backend.name
+    policy = jsonencode({
+        rules = [
+            {
+                rulePriority = 1
+                description = "Keep Last 10 Images"
+                selection = {
+                    tagStatus = "any"
+                    countType = "imageCountMoreThan"
+                    countNumber =  10
+                }
+                action = {
+                    type = "expire"
+                }
+            }
+        ]
+    })
+}
